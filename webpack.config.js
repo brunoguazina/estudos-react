67f11789a75d197eb51ff5873300403faa1229e3
brunoguazina <bruno@guazina.com>
2018-03-04 13:43:20 -0300

    incluindo babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  entry: './app.js',
-  output: {
-    filename: 'bundle.js'
-  }
-};
+    entry: './app.js',
+    output: {
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          }
+        }
+      ]
+    }
+  };
