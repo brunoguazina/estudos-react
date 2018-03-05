ac2bfb640e0258fad33a17eaaf00c4c1754adc51
brunoguazina <bruno@guazina.com>
2018-03-05 19:20:48 -0300

    incluindo css

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
+const styles = {
+    color: '#555',
+    
+}
+
 const MyComponent = () => { 
   return (
-    <div>
+    <div style={styles}>
       <h1>Hello World</h1>
       <p>
         This is my first example with <b>React</b> and <b>JSX</b>
